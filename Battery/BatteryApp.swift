185af5069848c545c4698ed9b928b4503b354292
yuppejp <yuppejp@gmail.com>
2022-09-17 08:24:53 +0900

    Initial Commit

---
+++ Battery/BatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryApp.swift
+//  Battery
+//
+//  Created by yukio on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct BatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Battery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Battery
+//
+//  Created by yukio on 2022/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
